c4fae22d53a2ca11d38eb068937cb548a8ff0ac6
nastya929 <dzusanastasia5@gmail.com>
2026-03-19 10:05:54 +0300

    2. Добавлен новый класс Event

---
+++ Calendar1/Calendar1/Event.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calendar1
+{
+    public class Event
+    {
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public Event(DateTime date, string description)
+        {
+            Date = date;
+            Description = description;
+        }
+    }
+
+}
